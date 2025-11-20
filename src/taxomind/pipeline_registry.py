e36f36ca088f563838d766f9ea5c15a11739606b
Gabriele Tani <gabriele.tani@gmail.com>
2025-11-20 18:25:00 +0100

    remo old training pipe registry

--- src/taxomind/pipeline_registry.py
+++ src/taxomind/pipeline_registry.py
@@ -6,7 +6,6 @@ from kedro.pipeline import Pipeline
 
 from taxomind.pipelines.taxonomy_pipes import pipeline as taxonomy_pipeline
 from taxomind.pipelines.zero_shot_pipes import pipeline as zero_shot_pipeline
-from taxomind.pipelines.supervised_pipes import pipeline as supervised_pipeline
 
 
 def register_pipelines() -> dict[str, Pipeline]:
@@ -18,12 +17,10 @@ def register_pipelines() -> dict[str, Pipeline]:
 
     taxonomy_pipe = taxonomy_pipeline.create_pipeline()
     zero_shot_pipe = zero_shot_pipeline.create_pipeline()
-    supervised_pipe = supervised_pipeline.create_pipeline()
 
     pipelines = {
         "taxonomy_pipe": taxonomy_pipe,
         "zero_shot_pipe": zero_shot_pipe,
-        "supervised_pipe": supervised_pipe,
     }
-    pipelines["__default__"] = taxonomy_pipe + zero_shot_pipe + supervised_pipe
+    pipelines["__default__"] = taxonomy_pipe + zero_shot_pipe
     return pipelines
