f9bd4796189c3a9edc8b083b8751d6c9eea16ac4
Gabriele Tani <gabriele.tani@gmail.com>
2026-01-09 13:01:26 +0100

    Refactor taxonomy pipeline: simplify nodes by removing unnecessary enrichment steps

--- src/taxomind/pipelines/taxonomy_pipes/pipeline.py
+++ src/taxomind/pipelines/taxonomy_pipes/pipeline.py
@@ -16,57 +16,17 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="taxonomy_validated",
                 name="load_taxonomy",
             ),
-            node(
-                func=nodes.add_unknowns,
-                inputs="taxonomy_validated",
-                outputs="taxonomy_table",
-                name="add_unknowns",
-            ),
-            node(
-                func=nodes.enrich_labels,
-                inputs="taxonomy_table",
-                outputs="taxonomy_enriched",
-                name="enrich_labels",
-            ),
+            
             node(
                 func=nodes.embed_taxonomy,
                 inputs={
-                    "taxonomy": "taxonomy_enriched",
+                    "taxonomy": "taxonomy_validated",
                     "model_name": "params:embedding.model_name",
                 },
                 outputs="taxonomy_embedded",
                 name="embed_taxonomy",
             ),
-            node(
-                func=nodes.build_full_paths,
-                inputs="taxonomy_enriched",
-                outputs="taxonomy_full_paths",
-                name="build_full_paths",
-            ),
-            node(
-                func=nodes.embed_full_paths,
-                inputs={
-                    "taxonomy": "taxonomy_full_paths",
-                    "model_name": "params:zero_shot.model_name",
-                },
-                outputs="taxonomy_full_path_embedded",
-                name="embed_full_paths",
-            ),
-            node(
-                func=nodes.build_flat_hierarchical_labels,
-                inputs="taxonomy_enriched",
-                outputs="taxonomy_flat_hierarchical",
-                name="build_flat_hierarchical_labels",
-            ),
-            node(
-                func=nodes.embed_flat_hierarchical_taxonomy,
-                inputs={
-                    "taxonomy": "taxonomy_flat_hierarchical",
-                    "model_name": "params:embedding.model_name",
-                },
-                outputs="taxonomy_flat_hierarchical_embedded",
-                name="embed_flat_hierarchical_taxonomy",
-            ),
+
 
         ]
     )
